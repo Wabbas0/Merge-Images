9308df9eb9ecd2c5ed83b15e5c347a39ce0da80b
waleed <waleed.gamal1000@gmail.com>
2022-09-21 09:38:34 +0200

    Refactored index.js

--- index.js
+++ index.js
@@ -1,57 +1,43 @@
-let { writeFile } = require('fs');
-let { join } = require('path');
-let request = require('request');
-let mergeImg = require('merge-img');
-let argv = require('minimist')(process.argv.slice(2));
+import { join } from "path";
+import fetch from "node-fetch";
+import mergeImg from "merge-img";
+import minimist from "minimist";
+
+let argv = minimist(process.argv.slice(2));
 
 let {
-    greeting = 'Hello', who = 'You',
-    width = 400, height = 500, color = 'Pink', size = 100,
+    greeting = "Hello",
+    who = "You",
+    width = 400,
+    height = 500,
+    color = "Pink",
+    size = 100,
 } = argv;
 
-let firstReq = {
-// https://cataas.com/cat/says/Hi%20There?width=500&amp;height=800&amp;c=Cyan&amp;s=150
-url: 'https://cataas.com/cat/says/' + greeting + '?width=' + width + '&height=' + height + '&color' + color + '&s=' + size, encoding: 'binary'
-};
+const getImageUrl = (name) =>
+    `https://cataas.com/cat/says/${name}?width=${width}&height=${height}&color=${color}&s=${size}`;
 
-let secondReq = {
-    url: 'https://cataas.com/cat/says/' + who + '?width=' + width + '&height=' + height + '&color' + color + '&s=' + size, encoding: 'binary'
-};
+const fileOut = join(process.cwd(), `/cat-card.jpg`);
 
-request.get(firstReq, (err, res, firstBody) => { 
-    if(err) {
-        console.log(err);
-        return; 
+const [image1, image2] = await Promise.all([
+    getPhotoFromAPI(getImageUrl(greeting)),
+    getPhotoFromAPI(getImageUrl(who)),
+]);
+
+const mergedImg = await mergeImg([
+    { src: image1, x: width, y: height },
+    { src: image2, x: width, y: height },
+]);
+
+mergedImg.write(fileOut, () => console.log("Saved!"));
+
+async function getPhotoFromAPI(url) {
+    try {
+        const response = await fetch(url);
+        const arrayBuffer = await response.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+        return buffer;
+    } catch (err) {
+        console.log("Error while fetching image from API", err);
     }
-    
-    console.log('Received response with status:' + res.statusCode);
-    
-    request.get(secondReq, (err, res, secondBody) => { 
-        if(err) {
-            console.log(err);
-            return; 
-        }
-        
-        console.log('Received response with status:' + res.statusCode); 
-
-        mergeImg([ 
-          { src: new Buffer(firstBody, 'binary'), x: 0, y:0 }, 
-          { src: new Buffer(secondBody, 'binary'), x: width, y: 0 }
-        ]).then(img => {
-          img.getBuffer('image/jpeg', (err, buffer) => {
-                if (err) {
-                  console.log(err)
-                }
-
-                const fileOut = join(process.cwd(), `/cat-card.jpg`);
-                
-                writeFile(fileOut, buffer, 'binary', (err) => { if(err) {
-                    console.log(err);
-                    return; 
-                }
-                
-                console.log("The file was saved!"); });
-              });
-            }); 
-        });
-});
+}
